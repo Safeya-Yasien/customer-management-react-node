72c6d203ea7692e40c98d46ec9eaf2202565ce6e
Safeya-Yasien <ssafeya5@gmail.com>
2025-11-13 09:06:57 +0200

    update roleMiddleware.ts remove console.log

--- server/src/middlewares/roleMiddleware.ts
+++ server/src/middlewares/roleMiddleware.ts
@@ -1,7 +1,4 @@
 const roleMiddleware = (roles: string[]) => (req: any, res: any, next: any) => {
-
-    console.log('User Role:', req.user.role);   
-    console.log('Required Roles:', roles);
   if (req.user.role.includes(roles)) {
     next();
   } else {

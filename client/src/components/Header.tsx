1849cc3df8ec01952b7b709a278bf3e45947476a
Safeya-Yasien <ssafeya5@gmail.com>
2025-11-13 12:37:44 +0200

    update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 const Header = () => {
   return (
     <header className=" text-white flex items-center justify-between">
-      <h2 className="text-2xl ">My Customers</h2>
+      <h2 className="text-2xl ">DashCRM</h2>
 
       <SearchBar />
     </header>
